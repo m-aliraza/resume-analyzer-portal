0836dbe6b027b309ac8683edf4753c01c940108b
m-aliraza-pgs <ali.raza@epharmaguide.com>
2025-11-16 16:39:03 +0500

    Initial commit

---
+++ src/main/java/com/resume_analyzer/ResumeAnalyzerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.resume_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAnalyzerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAnalyzerPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume_analyzer/ResumeAnalyzerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
